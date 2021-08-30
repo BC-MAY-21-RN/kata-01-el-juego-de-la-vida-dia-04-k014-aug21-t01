7b22f1721907285d674329db9f336febacf8784b
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-08-30 13:12:46 -0500

    Add tests for game of life
     Co-authored-by: brayandiazc <brayandiazc@gmail.com>

---
+++ __test__/board.test.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+import { Cell } from "../src/cell.js";
+import { LIVE, DEAD } from "../src/state.js";
+import { Board } from "../src/board.js";
+import { validCoords, invalidCoords, neighbours } from "../src/utils/coord.js";
+
+describe("board", () => {
+  let board;
+
+  beforeEach(async () => {
+    let firstGeneration = [
+      [new Cell(DEAD), new Cell(DEAD)],
+      [new Cell(LIVE), new Cell(DEAD)],
+    ];
+    board = new Board(firstGeneration);
+  });
+
+  test("should create a board", () => {
+    const expectedBoard = [
+      [new Cell(DEAD), new Cell(DEAD)],
+      [new Cell(LIVE), new Cell(DEAD)],
+    ];
+    expect(board.getMatrix()).toStrictEqual(expectedBoard);
+  });
+
+  describe("cell position", () => {
+    test("should belongs to board", () => {
+      validCoords.forEach((coord) => {
+        const [x, y] = coord;
+        expect(board.isCoordValid({ x, y })).toBeTruthy();
+      });
+    });
+
+    test("shouldn't belongs to board", () => {
+      invalidCoords.forEach((coord) => {
+        const [x, y] = coord;
+        expect(board.isCoordValid({ x, y })).toBeFalsy();
+      });
+    });
+  });
+
+  test("should get a neighbours", () => {
+    const neighs = neighbours({ row: 1, col: 1 });
+    const expectNeighs = [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+      [1, 0],
+      [1, 2],
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ];
+    expect(neighs).toStrictEqual(expectNeighs);
+  });
+
+  test("should get a cell", () => {
+    const cell = board.getCell({ x: 1, y: 0 });
+    expect(cell.isAlive());
+  });
+
+  test("should count live neighbours", () => {
+    const liveNeighbours = board.countLiveNeighbours({ x: 0, y: 0 });
+    expect(liveNeighbours).toBe(1);
+  });
+});
